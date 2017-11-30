37a09bd823540f7864e39ce94084dea36419d63b
William Elliott <wcelliott1991@gmail.com>
2017-11-30 11:17:07 -0500

    firebase db now updates with user specific goals

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import AddGoal from './addGoal';
 
 import '../App.css';
 
@@ -12,7 +13,9 @@ class App extends Component {
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/TheHomeDepot.svg/1000px-TheHomeDepot.svg.png" className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Goal Coach</h1>
         </header>
-        <h1>Hey there!</h1>
+        <h3>Goals</h3>
+        <AddGoal/>
+        <div>Goals List</div>
         <div><Link to={'/signin'}>Sign in</Link></div>
         <div><Link to={'/signup'}>Sign up</Link></div>
       </div>
